91bc5b7da56ce92b873405d9faed63ec541015ff
Jaswanth Reddy <jaswanthreddyam@gmail.com>
2026-07-16 10:43:33 +0530

    fix: resolve alembic multiple heads error

--- backend/alembic/versions/3fa79271cad2_normalize_seed_item_types.py
+++ backend/alembic/versions/3fa79271cad2_normalize_seed_item_types.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '3fa79271cad2'
-down_revision: Union[str, None] = 'c3d4e5f6a7b8'
+down_revision: Union[str, None] = '689fbcdb4541'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
